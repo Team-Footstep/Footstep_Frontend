fb3300a78d514f898bbe6794fc0b48fda4aad6c8
IBORY-PURPLE <lala1522@gachon.ac.kr>
2022-07-27 15:26:15 +0900

    final disign setting

--- footstep/src/components/ProfileSetting/textbox.js
+++ footstep/src/components/ProfileSetting/textbox.js
@@ -3,16 +3,26 @@ import React from "react";
 import styles from "../ProfileSetting/textbox.module.css";
 
 function TextBox({ isSpecial, value }) {
+  const [write, setWrite] = React.useState([]);
+  const onChange = (event) => {
+    setWrite(event.target.value);
+  };
+  console.log(write);
+  //"저장하기" 버튼 클릭시 입력된 데이터를 전송
   return (
     <div>
       {isSpecial ? (
         <input
+          onChange={onChange}
+          value={write}
           className={`${value ? styles.name_job : styles.email_code}`}
           placeholder="작성해주시오..."
           type="text"
         />
       ) : (
         <input
+          onChange={onChange}
+          value={write}
           className={styles.aboutme}
           placeholder="작성해주시오..."
           type="text"
