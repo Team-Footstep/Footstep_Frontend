a15bb784e63e59a52fd4a02f022fd2247a2f4286
seorin317 <dja0317@naver.com>
2022-07-27 13:30:13 +0900

    커밋

--- footstep/src/test/Test.js
+++ footstep/src/test/Test.js
@@ -1,11 +1,15 @@
 import React from "react";
+// import SearchBar from '../components/SearchBar/SearchBar';
+import SearchCard from "../components/SearchCard/SearchCard";
+//import "./css/reset.css";
+//import "./css/variables.css";
 
 function Test () {
   return (
-    <div>
-      <h1>TEST PAGE</h1>
-    </div>
+    //<SearchBar/>
+    <SearchCard/>
   )
 }
 
 export default Test;
+  
