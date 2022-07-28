20469063d0377fbd3ff81dad1d576dfe55a59e71
IBORY-PURPLE <lala1522@gachon.ac.kr>
2022-07-28 21:18:16 +0900

    ProfileSetting feature finish

--- footstep/src/components/ProfileSetting/ProfileSetting.js
+++ footstep/src/components/ProfileSetting/ProfileSetting.js
@@ -1,81 +1,165 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../ProfileSetting/ProfileSetting.module.css";
-import TextBox from "../ProfileSetting/textbox.js";
-// import styles from "../ProfileSetting/Button.module.css";
+import { Link } from "react-router-dom";
 
 function ProfileSetting({ image }) {
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const [values, setValues] = useState({
+    name: "",
+    job: "",
+    about_me: "",
+    email: "",
+    code: "",
+  });
+
+  const onChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
   };
-  function resetBtn() {
-    "#tmpSendFrm"[0].reset();
-  }
+
+  const onSubmit = (event) => {
+    // event.preventDefault();
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  const onClick = () => {
+    setValues("");
+    //변경하기를 클릭할시 input안의 state value값을 초기화 하고싶다!!!
+  };
+
+  // const obj = JSON.parse(values);
+
+  // console.log(
+  //   values.name,
+  //   values.job,
+  //   values.about_me,
+  //   values.email,
+  //   values.code
+  // );
 
   return (
     <div className={styles.body}>
-      <form
-        id="profilesetting"
-        onSubmit={onSubmit}
-        action="/Users/mac/Woowoo/Footstep_Frontend/footstep/src/routes/ProfileSetting/ProfileSetting.js"
-        clasName={styles.container}
-        method="post"
-      >
+      <div className={styles.container}>
         <div className={styles.profile_container}>
           <img src={image} className={styles.image} />
-          <div className={styles.textbox_bundle}>
-            <div className={styles.name}>
-              <p>이름</p>
-              <TextBox isSpecial={true} value={1} />
-            </div>
-            <div className={styles.job}>
-              <p>직업</p>
-              <TextBox isSpecial={true} value={1} />
-            </div>
-            <div className={styles.about_me}>
-              <p>자기소개</p>
-              <TextBox isSpecial={false} />
-            </div>
-          </div>
-          <div className={styles.button_bundle}>
-            <div className={styles.button_cancle}>
-              <button type="button" className={styles.cancle}>
-                취소하기
-              </button>
-            </div>
-            <div className={styles.button_save}>
-              <button type="submit" className={styles.save}>
-                저장하기
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className={styles.email_container}>
-          <div className={styles.email_container_content}>
-            <div className={styles.email_certifi_code}>
-              <div className={styles.email}>
-                <p>이메일</p>
-                <TextBox isSpecial={true} value={0} />
+          <form
+            action=""
+            method="post"
+            name="Profile_info"
+            className={styles.profile_form_container}
+            onSubmit={onSubmit}
+          >
+            <div className={styles.profile_textbox}>
+              <div>
+                <label htmlFor="name">이름</label>
+                <input
+                  type="text"
+                  id="name"
+                  className={styles.name}
+                  name="name"
+                  value={values.name}
+                  placeholder="이름을 작성해 주세요..."
+                  onChange={onChange}
+                ></input>
               </div>
-              <div className={styles.button_certifi_code}>
-                <button type="button" className={styles.code}>
-                  인증코드 보내기
+              <div>
+                <label htmlFor="job">직업</label>
+                <input
+                  type="text"
+                  id="job"
+                  className={styles.job}
+                  name="job"
+                  value={values.job}
+                  placeholder="직업을 작성해 주세요..."
+                  onChange={onChange}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="about_me">자기소개</label>
+                <input
+                  type="text"
+                  id="about_me"
+                  className={styles.about_me}
+                  name="about_me"
+                  value={values.about_me}
+                  placeholder="자기소개를 작성해 주세요..."
+                  onChange={onChange}
+                ></input>
+              </div>
+            </div>
+            <div className={styles.profile_button_bundle}>
+              <div>
+                <button type="button" className={styles.cancle}>
+                  <Link to="/">
+                    <span className={styles.text_cancle}>취소하기</span>
+                  </Link>
+                </button>
+              </div>
+              <div>
+                <button className={styles.save}>
+                  <span className={styles.text_save}>저장하기</span>
                 </button>
               </div>
             </div>
-            <div className={styles.enter_code_change}>
-              <div className={styles.enter_code}>
-                <p>코드 입력</p>
-                <TextBox isSpecial={true} value={0} />
+          </form>
+        </div>
+        <div className={styles.email_Authen_container}>
+          <form
+            action=""
+            method="post"
+            name="Email_Authentication"
+            onSubmit={onSubmit}
+            className={styles.email_form_container}
+          >
+            <div className={styles.emailtextbox_sendbutton}>
+              <div>
+                <label htmlFor="email">이메일</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className={styles.email}
+                  value={values.email}
+                  placeholder="e-mail을 작성해 주세요..."
+                  onChange={onChange}
+                ></input>
               </div>
-              <div className={styles.button_change}>
-                <button onClick={resetBtn} className={styles.change}>
-                  변경하기
+              <div>
+                <button className={styles.send_email}>
+                  <span className={styles.text_send_email}>
+                    인증코드 보내기
+                  </span>
                 </button>
               </div>
             </div>
-          </div>
+
+            <div className={styles.codetextbox_changebutton}>
+              <div>
+                <label htmlFor="code">코드입력</label>
+                <input
+                  type="number"
+                  id="code"
+                  name="code"
+                  className={styles.code}
+                  value={values.code}
+                  placeholder="코드를 입력해 주세요..."
+                  onChange={onChange}
+                ></input>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  onClick={onClick}
+                  className={styles.change}
+                >
+                  <span className={styles.text_change}>변경하기</span>
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
