8e90aedbe357bdf7f6900dbc8442ef5758a2ef53
eunmi <eunmi208@konkuk.ac.kr>
2022-07-28 22:42:33 +0900

    [feat] Login Page 구현

--- footstep/src/routes/Login/Login.js
+++ footstep/src/routes/Login/Login.js
@@ -1,12 +1,31 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import SideBar from "../../components/SideBar/SideBar";
 import styles from "../Login/Login.module.css";
+import { useState } from "react";
+import LoginCard from "../../components/LoginCard/LoginCard";
 
-function Login () {
-  return (
-    <div>
-      <h1>login page</h1>
-    </div>
-  )
+function Login() {
+    const [open, setOpen] = useState(false);
+    const sideBarHandler = () => {
+        setOpen((prev) => !prev);
+    };
+    return (
+        <div>
+            <Header state={open} clickFunc={sideBarHandler} />
+            <div className={styles.contents}>
+                <SideBar
+                    img={null}
+                    name={"문비"}
+                    job={"프론트앤드 디자이너"}
+                    footprint={2000}
+                    display={!open}
+                    login={false}
+                />
+                <LoginCard />
+            </div>
+        </div>
+    );
 }
 
 export default Login;
