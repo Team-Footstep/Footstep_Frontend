cbe23388fd491972360b5972ee5910d3aba066b9
IBORY-PURPLE <lala1522@gachon.ac.kr>
2022-08-05 19:56:45 +0900

    [initial commit]

--- footstep/src/test/Test.js
+++ footstep/src/test/Test.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/Header/Header.js";
+import SideBar from "../components/SideBar/SideBar.js";
 
 function Test() {
-    return (
-        <div>
-            <h1>TEST PAGE</h1>
-        </div>
-    );
+  const [open, setOpen] = useState(true);
+  const sideBarHandler = () => {
+    setOpen((prev) => !prev);
+  };
+  return (
+    <div>
+      <Header state={open} clickFunc={sideBarHandler} icon={true} />
+      <div>
+        <SideBar
+          img={null}
+          name={"문비"}
+          job={"프론트앤드 디자이너"}
+          footprint={2000}
+          display={!open}
+          login={false}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Test;
