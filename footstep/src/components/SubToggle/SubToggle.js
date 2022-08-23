5ee481ac04818559a2e7126f64c9c24f156f73cd
eunmi <eunmi208@konkuk.ac.kr>
2022-08-24 05:55:05 +0900

    [fix] sidebar 내의 subtoggle 디자인 수정

--- footstep/src/components/SubToggle/SubToggle.js
+++ footstep/src/components/SubToggle/SubToggle.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import styles from "../SubToggle/SubToggle.module.css"
+import styles from "../SubToggle/SubToggle.module.css";
 
-function SubToggle ({title, number}) {
+function SubToggle({ title, number }) {
   const [openSub, setOpenSub] = useState(false);
   const subPageHandler = () => {
     setOpenSub((prev) => !prev);
   };
   return (
     <div className={styles.menu}>
-      <button onClick={subPageHandler}>{title}</button>
+      <div className={styles.title}>
+        <div
+          className={`${openSub ? styles.open : styles.close}`}
+          onClick={subPageHandler}
+        ></div>
+        <a>{title}</a>
+      </div>
       <div className={`${styles.menu_detail} ${openSub ? null : styles.blind}`}>
-        <a className={styles.detail}>바로가기 기록</a>
+        <li>
+          <a className={styles.detail}>바로가기 기록</a>
+        </li>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default SubToggle;
